ff8e7a52e6166ad31450234298e51ed1ca4a82ef
Vincent Bailey Jr <vincentbaileyjr@Vincents-MacBook-Pro-2.local>
2021-08-23 21:21:29 -0500

    First revision of Hello World script. Added more text and a shape. Hooray for shapes!

--- Greetings.py
+++ Greetings.py
@@ -1,1 +1,11 @@
-print("Howdy World!")
+# This is a script that simply prints to screen that infamous programming
+# line. You know the one.
+
+print("Hello World!")
+print("That sounds so formal, doesn't it? Howdy World! Eh, that's a little better...")
+print("Anyway, here's a triangle for your trouble. See ya around!!")
+
+print("   /\   ")
+print("  /  \ ")
+print(" /    \ ")
+print("/______\ ")
