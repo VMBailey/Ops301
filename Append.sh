73fe1b27effe5a349379918b3cdec6bfa0e6c7a3
Vincent <vmarshallbailey@gmail.com>
2021-09-01 06:56:39 +0000

    A script that copies the system log file to the user's current directory.

---
+++ Append.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Script Name: Append
+# Author: Vincent Bailey
+# Last Revision: 8/30/2021
+
+date=$(date + "%T")
+
+echo "Alright, I'll start copying the Syslog for ya! One sec..."
+cp -r /var/log/syslog C:\Users\Mr.Bloodedge\Documents
+sleep 1.8
+
+echo "Moving over to the Documents folder really quick..."
+cd C:\Users\Mr.Bloodedge\Documents
+sleep 1.8
+
+echo "Thanks for being so patient, by the way."
+sleep 1.8
+
+echo "Just adding the date and time to the filename now..."
+mv syslog syslog "$(date + "%Y%m%d_%T")"
+sleep 1.8
+
+echo "All done!"
