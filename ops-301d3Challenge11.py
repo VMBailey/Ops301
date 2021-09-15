e4fb1738522c53673cf44a371ae1a9cedd4f2b12
VMBailey <vmarshallbailey@gmail.com>
2021-09-15 06:57:06 +0000

    Wrapping up this script now.

---
+++ ops-301d3Challenge11.py
@@ -0,0 +1,11 @@
+# Script Name:                     Python File Handling
+# Author:                          Vincent Bailey
+# Date of Latest Revision:         9/14/2021
+# Purpose:                         Finishing up now
+#                                  
+
+
+import os
+
+newfile = open("newtextfile.txt", "w")
+text = ["Till I find the righteous one \n", "We got fun & games \n" "Purple Rain"]
