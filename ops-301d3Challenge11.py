eb88fb8570bda216309c7ad33381d2657be90576
VMBailey <vmarshallbailey@gmail.com>
2021-09-15 07:55:17 +0000

    First rendition of a script that creates a new file, writes data to that file, and then deletes it.

--- ops-301d3Challenge11.py
+++ ops-301d3Challenge11.py
@@ -1,11 +1,21 @@
 # Script Name:                     Python File Handling
 # Author:                          Vincent Bailey
 # Date of Latest Revision:         9/14/2021
-# Purpose:                         Finishing up now
-#                                  
+# Purpose:                         This script writes a haiku into a text file, prints the first
+#                                  line into the user's terminal and then deletes the text file.
 
 
-import os
+from os import remove
 
 newfile = open("newtextfile.txt", "w")
-text = ["Till I find the righteous one \n", "We got fun & games \n" "Purple Rain"]
+newfile = open("newtextfile.txt", "a")
+
+
+newfile.write("A dance of salt and sweet, \n")
+newfile.write("Betwixt two mountains of yeast. \n")
+newfile.write("The Fountain of Youth.")
+
+
+print ("A dance of salt and sweet,")
+
+remove("newtextfile.txt")
